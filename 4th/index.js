c2560f45d890c8719718923f47e4cd1e4076f37e
규태 변 <byungt02@naver.com>
2022-10-17 09:59:16 +0900

    [GT]사소한 수정

--- 4th/index.js
+++ 4th/index.js
@@ -1,11 +1,16 @@
 var year = new Date().getFullYear();
 var month = new Date().getMonth();
-var rest = [[10, 3], [10, 9]]
+var rest = [[10, 3], [10, 9], [1, 1], [3, 1], [5, 5], [6, 6], [8, 15], [12, 25]]
+var isNext = [true, true, false, true, true, false, true, false];
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
 
 
 function ResetCalendar()
 {
     var current = new Date(year, month);
+    var monthDiv = document.getElementById("month");
+    monthDiv.innerHTML = "<b>" + monthNames[current.getMonth()] + " " + year.toString() + "</b>";
     var firstDay = current.getDay();
     var coord = [0, firstDay];
     var day = 1;
@@ -25,21 +30,44 @@ function ResetCalendar()
                 break;
         }
 
-        if (type == 0)
+        switch (type)
         {
-            pos.innerHTML += "<br>&nbsp개천절";
-        }
-        if (type == 1)
-        {
-            pos.innerHTML += "<br>&nbsp한글날";
+            case 0:
+                pos.innerHTML += "<br>&nbsp개천절";
+                break;
+            case 1:
+                pos.innerHTML += "<br>&nbsp한글날";
+                break;
+            case 2:
+                pos.innerHTML += "<br>&nbsp신정";
+                break;
+            case 3:
+                pos.innerHTML += "<br>&nbsp3·1절";
+                break;
+            case 4:
+                pos.innerHTML += "<br>&nbsp어린이날";
+                break;
+            case 5:
+                pos.innerHTML += "<br>&nbsp현충일";
+                break;
+            case 6:
+                pos.innerHTML += "<br>&nbsp광복절";
+                break;
+            case 7:
+                pos.innerHTML += "<br>&nbsp기독탄신일";
+                break;
         }
 
-        if (type != rest.length)
+        if (isNext[type])
         {
             if (pos.className != "sund")
                 pos.className = "sund";
             else
                 nextRest += 1;
+        }
+        else if (type < rest.length)
+        {
+            pos.className = "sund";
         }
 
         if (nextRest != 0 && pos.className != "sund")

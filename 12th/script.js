245e7d96cb9eb74d0c0a8897ab4ac0b9f49da963
규태 변 <byungt02@naver.com>
2022-11-21 11:49:36 +0900

    [GT]애니메이션이 제일 고민

--- 12th/script.js
+++ 12th/script.js
@@ -1,8 +1,47 @@
+const myKey =
+{
+    LEFT: 37,
+    RIGHT: 39,
+    DOWN: 40,
+    UP: 38
+};
+
+var pos = ["2.34375vmin", "21.6796875vmin", "41.015625vmin", "60.3515625vmin"];
+
+var map =
+[[1, 0, 0, 0],
+[0, 0, 0, 0],
+[0, 0, 0, 0],
+[0, 0, 0, 0]];
+
 const {createApp} = Vue;
 createApp({
     data(){
         return {
-            bigPlates : 16,
+            bigPlates: 16,
         }
     }
 }).mount("#base");
+
+function keyInput(e)
+{
+    var block = document.getElementsByClassName("top")[0];
+    switch(e.keyCode)
+    {
+        case myKey.LEFT:
+            block.style.left = pos[0];
+            break;
+        case myKey.RIGHT:
+            block.style.left = pos[3];
+            break;
+        case myKey.DOWN:
+            block.style.top = pos[3];
+            break;
+        case myKey.UP:
+            block.style.top = pos[0];
+            break;
+    }
+};
+
+window.onkeydown = keyInput;
+
